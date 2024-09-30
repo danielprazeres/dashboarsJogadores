4f6a635d6ee141fddddd2cf9c6bba306b187e399
Daniel Prazeres <51274652+danielprazeres@users.noreply.github.com>
2024-09-30 18:15:25 -0300

    Update 1_🏠_home.py
    Correção do botão kaggle

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -19,10 +19,10 @@ if "data" not in st.session_state:
 st.markdown("# FIFA 2023 OFFICIAL DATASET ⚽️")
 st.sidebar.markdown("Desenvolvido por [Daniel Prazeres](https://danielprazeres.com)")
 
-btn = st.button("Acesse os dados no Kaggle")
-
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+btn = st.link_button(
+    "Acesse os dados no Kaggle", 
+    "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
+)
 
 st.markdown(
     """
